6cc35e6690c9e86f5513a5e78154aceb8ad6515d
DaniPazLor <85082509+DaniPazLor@users.noreply.github.com>
2022-01-22 20:42:11 +0100

    Animados botones activity_europa.java

--- app/src/main/java/com/practicas/midletatlas/MainActivity.java
+++ app/src/main/java/com/practicas/midletatlas/MainActivity.java
@@ -14,10 +14,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void pasarActividad (View view){
-        Intent intent = new Intent(this,activity_europa.class);
-        startActivity(intent);
+    public void onClickMostrarContinentes (View view){
+        Intent intent;
+
+        switch (view.getId()){
+
+            case R.id.btnEuropa:
+                intent = new Intent(this,activity_europa.class);
+                startActivity(intent);
+                break;
+            case R.id.btnAfrica:
+                intent = new Intent(this,activity_europa.class);
+                startActivity(intent);
+                break;
+            case R.id.btnAsia:
+                intent = new Intent(this,activity_europa.class);
+                startActivity(intent);
+                break;
+            case R.id.btnAmerica:
+                intent = new Intent(this,activity_europa.class);
+                startActivity(intent);
+                break;
+            case R.id.btnOceania:
+                intent = new Intent(this,activity_europa.class);
+                startActivity(intent);
+                break;
+        }
+
+
     }
+
 
 
 }
--- app/src/main/java/com/practicas/midletatlas/activity_europa.java
+++ app/src/main/java/com/practicas/midletatlas/activity_europa.java
@@ -2,13 +2,35 @@ package com.practicas.midletatlas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
 
 public class activity_europa extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_europa);
     }
+
+    public void finalizarActivity(View view) {
+        finish();
+    }
+
+    public void mostrarPaises(View view) {
+        RadioButton btnEspana = findViewById(R.id.radioButtonEsp);
+        if (btnEspana.isChecked()){
+
+        }
+
+        Intent intent = new Intent(activity_europa.this, activity_paises_europa.class);
+        startActivity(intent);
+
+    }
+
+
 }
