9c18fbdb13266f65f8d95efb5a0c98005c8072f0
DaniPazLor <85082509+DaniPazLor@users.noreply.github.com>
2022-01-23 18:40:27 +0100

    Añadido init con extras a activity_paises_europa.java

--- app/src/main/java/com/practicas/midletatlas/activity_europa.java
+++ app/src/main/java/com/practicas/midletatlas/activity_europa.java
@@ -23,11 +23,13 @@ public class activity_europa extends AppCompatActivity {
 
     public void mostrarPaises(View view) {
         RadioButton btnEspana = findViewById(R.id.radioButtonEsp);
-        if (btnEspana.isChecked()){
-
-        }
+        RadioButton btnSuiza = findViewById(R.id.radioButtonSui);
+        RadioButton btnHolanda = findViewById(R.id.radioButtonHol);
 
         Intent intent = new Intent(activity_europa.this, activity_paises_europa.class);
+        intent.putExtra("pais1", btnEspana.isChecked());
+        intent.putExtra("pais2", btnSuiza.isChecked());
+        intent.putExtra("pais3", btnHolanda.isChecked());
         startActivity(intent);
 
     }
--- app/src/main/java/com/practicas/midletatlas/activity_paises_europa.java
+++ app/src/main/java/com/practicas/midletatlas/activity_paises_europa.java
@@ -2,7 +2,10 @@ package com.practicas.midletatlas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableRow;
 
 public class activity_paises_europa extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class activity_paises_europa extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paises_europa);
+
+        Bundle datos = getIntent().getExtras();
+        TableRow tr1 = findViewById(R.id.tableRowP1);
+
+        if (datos.getBoolean("pais1")){
+            tr1.setVisibility(View.VISIBLE);
+        }
+
     }
 }
